8f63de9df1c813f1ce63104faadfc1b43eed5a05
siguo.duan@melot.cn <siguo.duan@melot.cn>
2020-01-08 14:14:02 +0800

    //Update constructors.

--- src/main/java/com/talkee/trace/TraceException.java
+++ src/main/java/com/talkee/trace/TraceException.java
@@ -4,26 +4,33 @@ import lombok.Data;
 
 /**
  * @author Duansg
- * @desc 自定义异常
- * @date 2019-12-16
+ * @desc Custom exception
+ * @date 2019-12-16 18:44:21
  */
 @Data
 public class TraceException extends RuntimeException {
     /**
-     * @desc 错误码
+     * @desc Exception code.
      */
     private Integer code;
     /**
-     * @desc 描述
+     * @desc Describe
      */
     private String msg;
-
+    /**
+     * @desc There are parameter constructors.
+     * @param code
+     * @param msg
+     */
     public TraceException(Integer code, String msg){
         super(msg);
         this.code = code;
         this.msg = msg;
     }
-
+    /**
+     * @desc There are parameter constructors.
+     * @param msg
+     */
     public TraceException(String msg){
         super(msg);
         this.msg = msg;
